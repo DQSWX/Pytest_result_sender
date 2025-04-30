b211cc92352b7f1d13a1a9f276be84820625e036
Joe <jadeyan1987@163.com>
2025-04-30 15:31:47 +0800

    使用pytest.ini配置邮件发送参数

--- src/pytest_result_sender/plugin.py
+++ src/pytest_result_sender/plugin.py
@@ -1,4 +1,4 @@
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 
 import pytest
 import smtplib
@@ -10,23 +10,42 @@ data = {
     "failed": 0,
 }
 
-def pytest_runtest_logreport(report:pytest.TestReport):
+
+def pytest_addoption(parser):
+    parser.addini(
+        'send_when',
+        help="何时发送邮件，every(每次都发送)，on_fail(失败时发送)"
+    )
+    parser.addini(
+        'receiver_email',
+        help="发到何处"
+    )
+
+
+def pytest_runtest_logreport(report: pytest.TestReport):
     # print(report)
     if report.when == 'call':
         print("本次用例的执行结果", report.outcome)
         data[report.outcome] += 1
-def pytest_collection_finish(session:pytest.Session):
+
+
+def pytest_collection_finish(session: pytest.Session):
     # 用例加载完成之后执行，包含全部的用例
-    data['total']=len(session.items)
-def pytest_configure():
+    data['total'] = len(session.items)
+
+
+def pytest_configure(config: pytest.Config):
     # 配置加载完毕后执行，所有测试用例执行前执行
-    data['start_time']=datetime.now()
+    data['start_time'] = datetime.now()
+    data['send_when'] = config.getini("send_when")
+    data['receiver_email'] = config.getini("receiver_email")
     # print(f"{datetime.now()} pytest开始执行")
+
 
 def pytest_unconfigure():
     # 配置卸载完毕后执行，所有测试用例执行后执行
     data['end_time'] = datetime.now()
-    data['duration'] = data['end_time']-data['start_time']
+    data['duration'] = data['end_time'] - data['start_time']
     data['pass-ratio'] = data['passed'] / data['total'] * 100
     data['pass-ratio'] = f"{data['pass-ratio']:.2f}%"
     assert timedelta(seconds=3) > data['duration'] > timedelta(seconds=2.5)
@@ -36,14 +55,19 @@ def pytest_unconfigure():
     assert data['pass-ratio'] == '66.67%'
     send_email()
 
-# 配置参数
-SMTP_SERVER = "smtp.163.com"
-SMTP_PORT = 465
-SENDER_EMAIL = "jadeyan1987@163.com"
-AUTH_CODE = "AKfk9qk8kHkeLHnx"  # 替换为实际授权码
-RECEIVER_EMAIL = "474626958@qq.com"
 
 def send_email():
+    # 配置参数
+    SMTP_SERVER = "smtp.163.com"
+    SMTP_PORT = 465
+    SENDER_EMAIL = "jadeyan1987@163.com"
+    AUTH_CODE = "AKfk9qk8kHkeLHnx"  # 替换为实际授权码
+    receiver_email = data['receiver_email']
+
+    if data['send_when'] == 'on_fail' and data['failed'] == 0:
+        return
+    if not data['receiver_email']:
+        return
     test_result = f"""
     测试时间：{data['end_time']}
     用例数量：{data['total']} 
@@ -55,14 +79,14 @@ def send_email():
     # 构建邮件对象
     msg = MIMEText(test_result, "plain", "utf-8")
     msg["From"] = formataddr(["自动化测试系统", SENDER_EMAIL])
-    msg["To"] = RECEIVER_EMAIL
-    msg["Subject"] = "【测试报告】2025-04-28 测试结果"
+    msg["To"] = receiver_email
+    msg["Subject"] = f"""【测试报告】{datetime.now()}测试结果"""
 
     try:
         # 使用 SSL 加密连接
         with smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT) as server:
             server.login(SENDER_EMAIL, AUTH_CODE)
-            server.sendmail(SENDER_EMAIL, [RECEIVER_EMAIL], msg.as_string())
+            server.sendmail(SENDER_EMAIL, [receiver_email], msg.as_string())
         print("邮件发送成功")
     except Exception as e:
         print(f"发送失败: {str(e)}")
